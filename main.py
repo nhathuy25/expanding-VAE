fef799b5f9dc4c3b20ce2baec368409faa725911
Huy NGUYEN <huu_nhat_huy.nguyen@insa-cvl.fr>
2024-12-04 19:03:46 +0100

    Adding main.py file to sumerize works

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ import json
 
 # configuration
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-INPUT_DIM = 784
-HIDDEN_DIM = 256
-LATENT_DIM = 32
 NUM_EPOCHS = 15
 LEARNING_RATE = 1e-3
 BATCH_SIZE = 32
@@ -26,30 +23,22 @@ data_transform = transforms.Compose([transforms.ToTensor()])
 dataset = datasets.MNIST(root='dataset/', train=True, transform=data_transform, download=True)
 
 train_loader = DataLoader(dataset=dataset, batch_size=BATCH_SIZE, shuffle=True)
-model = VAE(INPUT_DIM, HIDDEN_DIM, LATENT_DIM).to(DEVICE)
 
-encoder_config = [256, 128,  32]
-decoder_config = [128, 256, 784]
+encoder_config = [256, 256,  32]
+decoder_config = [256, 256, 784]
 
 # Set the name for the model for saving
-model_name = f'VAE_2hid-256-128_{NUM_EPOCHS}eps'
+model_name = f'VAE_2hid_{NUM_EPOCHS}eps'
 
 model = VAE_expanding((28, 28), device=DEVICE)
 model.construct(encoder_config, decoder_config, False)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-#Verify pixels values in data loader:
-train_loader = DataLoader(
-    dataset,
-    batch_size=BATCH_SIZE,
-)
-torch.manual_seed(5576)
-for (x, y) in train_loader:
-    print('Min Pixel Value: {} \nMax Pixel Value: {}'.format(x.min(), x.max()))
-    print('Mean Pixel Value {} \nPixel Values Std: {}'.format(x.float().mean(), x.float().std()))
-    break
-
+''' Define Loss functions for VAE 
+    - Reconstruction difference, we use the Binary Cross Entropy (BCE) loss
+    - Kull
+'''
 # BCE Loss
 BCE_loss = nn.BCELoss(reduction = 'sum')
 
@@ -60,15 +49,11 @@ def loss_function(x, x_recon, mean, log_var):
     KLD      = - 0.5 * torch.sum(1+ log_var - mean.pow(2) - log_var.exp())
     return reproduction_loss + KLD
 
-# Trainning
-start_time = time.time()
-
-train_loss_list = []
-model.train()
-for epoch in range(NUM_EPOCHS):
-    loop = tqdm(enumerate(train_loader), total=len(train_loader), desc=f'Epoch {epoch+1}/{NUM_EPOCHS}', leave=False)
+''' Trainning per epoch function
+'''
+def train(model, tqdm_loop, loss_function, optimizer):
     train_loss = 0
-    for batch_idx, (x, _) in loop:
+    for batch_idx, (x, _) in tqdm_loop:
         # flatten the imput image
         x = x.view(-1, 28*28)
         x = x.to(DEVICE)
@@ -76,11 +61,7 @@ for epoch in range(NUM_EPOCHS):
         x_recon, mu, log_var = model(x)
 
         # compute loss functions    
-        #total_loss = loss_function(x, x_recon, mu, log_var)
-        
-        reproduction_loss = BCE_loss(x_recon, x)
-        KL_div = - 0.5 * torch.sum(1+ log_var - mu.pow(2) - log_var.exp())
-        total_loss = reproduction_loss + KL_div
+        total_loss = loss_function(x, x_recon, mu, log_var)
 
         # backprop
         train_loss += total_loss.item()
@@ -91,9 +72,30 @@ for epoch in range(NUM_EPOCHS):
         total_loss.backward()
         optimizer.step()
 
-        loop.set_postfix(loss=train_loss/(batch_idx*BATCH_SIZE+1))
+        tqdm_loop.set_postfix(loss=train_loss/(batch_idx*BATCH_SIZE+1))
     
-    train_loss_list.append(train_loss / (batch_idx*BATCH_SIZE+1))
+    return train_loss/(batch_idx*BATCH_SIZE+1)
+
+''' Adding nodes to the expands layers
+'''
+
+
+''' 
+            -- TRAINING PART --
+'''
+
+
+start_time = time.time()
+
+train_loss_list = []
+model.train()
+for epoch in range(NUM_EPOCHS):
+
+    loop = tqdm(enumerate(train_loader), total=len(train_loader), desc=f'Epoch {epoch+1}/{NUM_EPOCHS}', leave=False)
+    train_loss = 0
+    train_loss = train(model, loop, loss_function, optimizer)
+    
+    train_loss_list.append(train_loss/len(train_loader))
 
 # Mark end time
 end_time = time.time()
